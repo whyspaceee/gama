e2c15c0f8632d9994a82913aeac5c40b89810892
fadalaudzaa <fada.laudza2101@mail.ugm.ac.id>
2023-03-06 12:42:26 +0700

    add 2 logo sources, config edit for new color, login page changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main':'#E01F3D',
+      },
+    },
   },
   plugins: [],
 };
