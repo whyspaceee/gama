f2cb3eb7c273cf3747022b1814fcc8a7c0a97684
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2023-03-19 21:19:27 +0700

    updated merchant regist

--- src/components/merchant/register/BasicInformation.tsx
+++ src/components/merchant/register/BasicInformation.tsx
@@ -1,26 +1,39 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { SearchBox } from "@mapbox/search-js-react";
+import {
+  MutableRefObject,
+  Ref,
+  RefObject,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { FieldValues, useForm, UseFormRegister } from "react-hook-form";
+import { Map, MapRef } from "react-map-gl";
 import { z } from "zod";
+import { env } from "../../../env.mjs";
 import InputField from "./InputField";
 
 export type InputProps = {
   register: UseFormRegister<FieldValues>;
   errors: any;
   name: string;
-  label: string
+  label: string;
 };
 
-export default function MerchantBasicInformation({ setFormData, formData, setActiveIndex }: any) {
-    const phoneRegExp =
+export default function MerchantBasicInformation({
+  setFormData,
+  formData,
+  setActiveIndex,
+}: any) {
+  const phoneRegExp =
     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-  
+
   const validationSchema = z.object({
     title: z.string().min(3, { message: "Must be 3 characters" }).max(32),
     address: z.string().min(2, { message: "Must be 2 characters" }).max(32),
     number: z.string().regex(phoneRegExp, { message: "Invalid phone number" }),
   });
-
-
 
   const {
     register,
@@ -29,10 +42,19 @@ export default function MerchantBasicInformation({ setFormData, formData, setAct
     formState: { errors },
   } = useForm({ resolver: zodResolver(validationSchema) });
 
-  const onSubmit = (data: any) => { 
-    setFormData({...formData, ...data})
-    setActiveIndex(1)
+  const onSubmit = (data: any) => {
+    setFormData({ ...formData, ...data });
+    setActiveIndex(1);
   };
+
+  const [position, setPosition] = useState<GeolocationPosition>();
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(setPosition);
+  }, []);
+
+  const mapRef = useRef<MapRef>();
 
   return (
     <main className=" flex min-h-screen flex-col items-center justify-between  px-8 py-16 ">
@@ -42,22 +64,56 @@ export default function MerchantBasicInformation({ setFormData, formData, setAct
       </div>
       <form
         id="basic-information"
-        className=" flex w-full flex-col items-center gap-y-4 h-full  "
+        className=" flex h-full w-full flex-col items-center gap-y-4  "
         onSubmit={handleSubmit(onSubmit)}
       >
-        <p className=" text-xl font-bold text-center">Please fill in your basic information</p>
-        <InputField register={register} errors={errors} name="title" label='Merchant Name'/>
-        <InputField register={register} errors={errors} name="address" label='address'/>
-        <InputField register={register} errors={errors} name="number" label='number'/>
+        <p className=" text-center text-xl font-bold">
+          Please fill in your basic information
+        </p>
+        <InputField
+          register={register}
+          errors={errors}
+          name="title"
+          label="Merchant Name"
+        />
+        <InputField
+          register={register}
+          errors={errors}
+          name="address"
+          label="address"
+        />
+        <InputField
+          register={register}
+          errors={errors}
+          name="number"
+          label="number"
+        />
+        <div className="mt-4 overflow-hidden rounded-xl w-full relative bg-red">
+          <Map
+            attributionControl={false}
+            mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_TOKEN}
+            initialViewState={{
+              longitude: 110.376352,
+              latitude: -7.771081,
+              zoom: 12,
+            }}
+            style={{
+              position: "relative",
+              width: "100%",
+              height: "192px",
+            }}
+            mapStyle="mapbox://styles/mapbox/streets-v12"
+          ></Map>
 
+        </div>
       </form>
       <button
-          className=" mt-16  h-14 w-full rounded-xl bg-main text-lg font-bold text-white"
-          type="submit"
-          form="basic-information"
-        >
-          Continue
-        </button>
+        className=" mt-16  h-14 w-full rounded-xl bg-main text-lg font-bold text-white"
+        type="submit"
+        form="basic-information"
+      >
+        Continue
+      </button>
     </main>
   );
 }
